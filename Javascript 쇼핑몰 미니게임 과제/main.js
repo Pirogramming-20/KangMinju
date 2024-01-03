50793f0cf33fc1533e6fded6a362473114323462
minju <iaki111@naver.com>
2024-01-04 00:39:08 +0900

    JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -1,13 +1,26 @@
 function loadItems() {
     return fetch('./data.json')
-    .then(Response => Response.json())
-    .then(json => json.items);
+        .then(Response => Response.json())
+        .then(json => json.items);
 }
 
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('')
+}
+
+function createHTMLString(item) {
+    return `
+    <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+            <span class="item_description">${item.gender}, ${item.size}</span>
+        </li>    
+    `;
+}
 loadItems()
     .then(items => {
         console.log(items);
-        //displayItems(items);
+        displayItems(items);
         //setEventListeners(items);
     })
     .catch(console.log);
