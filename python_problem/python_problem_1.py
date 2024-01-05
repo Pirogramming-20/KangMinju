bcc94c7dfa3122995fc3a5c3f6c9bcaddc28589b
minju <iaki111@naver.com>
2024-01-05 16:14:20 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,14 @@
 num = 0
 
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+while num < 31 :
+    while True:
+        try:
+            count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+            
+            if count not in [1,2,3]:
+                print("1,2,3 중 하나를 입력하세요")
+                continue
+            break
+        except IntegerError:
+            print("정수를 입력하세요")
+        
