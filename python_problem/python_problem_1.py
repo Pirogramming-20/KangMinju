eeb10bff97be34beb94326d61047a6ebab18f19b
minju <iaki111@naver.com>
2024-01-05 16:24:34 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,17 +1,18 @@
 num = 0
 
-while num < 31 :
-    while True:
-        try:
-            count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-            
-            if count not in [1,2,3]:
-                print("1,2,3 중 하나를 입력하세요")
-                continue
-            break
-        except IntegerError:
-            print("정수를 입력하세요")
-        
-    for i in range(count):
-        num += 1
-        print(f"playerA : {num}")
+while num < 31:
+    for player in ["playerA", "playerB"]:
+        while True:
+            try:
+                count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+                
+                if count not in [1, 2, 3]:
+                    print("1, 2, 3 중 하나를 입력하세요")
+                    continue
+                break
+            except ValueError:
+                print("정수를 입력하세요")
+
+        for i in range(count):
+            num += 1
+            print(f"{player} : {num}")
