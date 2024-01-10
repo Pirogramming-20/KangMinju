748ea5bf6dcd1aa99215052fb83327b53b05eccf
minju <iaki111@naver.com>
2024-01-10 16:45:29 +0900

    Python 시작하기

---
+++ djangogirls/python_intro.py
@@ -0,0 +1,31 @@
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+hi()
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+
+hi("Ola")
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+hi("Rachel")
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+    
+for i in range(1, 6):
+    print(i)
